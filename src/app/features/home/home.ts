51affd6b6a59d754d53f5a1b98403aed9259fc35
Yusuf Umer <yusufumer26@gmail.com>
2026-08-15 21:37:41 +0300

    feat(home): add product category browsing

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -16,12 +16,12 @@ interface Category {
   styleUrl: './home.scss',
 })
 export class Home {
-  categories: Category[] = [
-    { name: 'T-Shirts', description: 'Explore T-Shirts', image: 'images/images.jpg', filter: 'Apparel' },
-    { name: 'Jeans', description: 'Explore Jeans', image: 'images/images (5).jpg', filter: 'Jeans' },
-    { name: 'Jackets', description: 'Explore Jackets', image: 'images/images (6).jpg', filter: 'Jackets' },
-    { name: 'Dresses', description: 'Explore Dresses', image: 'images/images (7).jpg', filter: 'Dresses' },
-    { name: 'Hoodies', description: 'Explore Hoodies', image: 'images/images (8).jpg', filter: 'Hoodies' },
-    { name: 'Suits', description: 'Explore Suits', image: 'images/images (9).jpg', filter: 'Suits' },
-  ];
+categories: Category[] = [
+  { name: 'T-Shirts', description: 'Explore T-Shirts', image: 'images/images.jpg', filter: 'Apparel' },
+  { name: 'Jeans', description: 'Explore Jeans', image: 'images/images (5).jpg', filter: 'Jeans' },
+  { name: 'Jackets', description: 'Explore Jackets', image: 'images/images (6).jpg', filter: 'Jackets' },
+  { name: 'Dresses', description: 'Explore Dresses', image: 'images/images (7).jpg', filter: 'Dresses' },
+  { name: 'Hoodies', description: 'Explore Hoodies', image: 'images/images (8).jpg', filter: 'Hoodies' },
+  { name: 'Suits', description: 'Explore Suits', image: 'images/images (9).jpg', filter: 'Suits' },
+];
 }
